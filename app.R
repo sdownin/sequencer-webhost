9509a91ebc2c8c9891fa1e96490fba497e45a7fd
Stephen Downing <stephendowning2008@gmail.com>
2022-07-28 23:30:41 -0500

    first commit in webhost subproject of sequencer app

---
+++ app.R
@@ -0,0 +1,21 @@
+##
+## APP ENTRY SCRIPT
+##
+##  - called by run.vbs script
+##
+
+## BASE LIBRARIES
+library(shiny)
+library(shinycssloaders)
+library(shinyjqui)
+library(dplyr)
+# library(tidyverse)
+# library(TraMineR)
+
+## APP MODULES
+source('analysisModule.R')  ## loads ui,server helper functions
+source('appUi.R') 		## loads `.ui()` function that returns `ui` object
+source('appServer.R')   ## loads `.server()` function that returns `server(input, output, session)` function
+
+## RUN APP
+shinyApp(ui=.ui(), server=.server(), enableBookmarking = "url")  ## enableBookmarking = "server", "url"
